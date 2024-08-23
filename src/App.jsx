f9bcb5175ca08fa020787d0b66542186da71c7ab
Guilherme Westphall <guilhermewestphall@gmail.com>
2024-08-23 15:51:46 -0300

    TODO

--- src/App.jsx
+++ src/App.jsx
@@ -24,14 +24,17 @@ export default function App() {
         document.documentElement.setAttribute('data-theme', dark ? 'dark' : 'light');
     }, [dark]);
 
-
+    // TODO: Criar componente Home para configurar todas as Sections visíveis na tela inicial
+    //       Passar as URLs ou Paths como argumento para cada Section
+    //       Realizar rederização dinâmica das Sections e vincular a cada Post
+    //       Agrupar Posts?   
 
     return (
         <Router >
             <div className="App" >
                 <Header handleDark={handleDark} />
                 <Routes className="main-content">
-                    <Route path="/" element={<Section name="Annotations" /> } />
+                    <Route path="/" element={<Section name="Annotations" /> } />    
                     <Route path="posts/:title" element={<Post />} />
                 </Routes>
                 <Footer />
