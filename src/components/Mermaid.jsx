5d41e45dd6486226eb2a47d92932d39be6630272
Guilherme Westphall <guilhermewestphall@gmail.com>
2024-09-04 17:11:55 -0300

    Renderizando Ok, mas n instantâneo

--- src/components/Mermaid.jsx
+++ src/components/Mermaid.jsx
@@ -2,13 +2,26 @@ import React, { useEffect, useRef } from "react";
 import mermaid from "mermaid";
 
 const Mermaid = ({ chart }) => {
+    const chartRef = useRef(null);
 
     useEffect(() => {
         mermaid.initialize({ startOnLoad: true });
-        mermaid.contentLoaded();
+    }, []);
+
+    useEffect(() => {
+        if (chartRef.current) {
+            chartRef.current.innerHTML = chart;
+            mermaid.render('mermaidChart', chart, (svgCode) => {
+                chartRef.current.innerHTML = svgCode;
+            });
+        }
     }, [chart]);
 
-    return <div className="mermaid">{chart}</div>;
+    return (
+        <>
+        <div className="mermaid" ref={chartRef} />
+        </>
+    );
 };
 
 export default Mermaid;
